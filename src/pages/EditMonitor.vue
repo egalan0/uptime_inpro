386e37e3392cfc36a83b95adc8e3be2a49086735
egalan0 <egalano@inprotelecom.com>
2026-03-02 17:39:47 -0600

    nombres

--- src/pages/EditMonitor.vue
+++ src/pages/EditMonitor.vue
@@ -3099,7 +3099,7 @@ message HealthCheckResponse {
 <?xml version="1.0" encoding="utf-8"?>
 <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
   <soap:Body>
-    <Uptime>Kuma</Uptime>
+    <Uptime>InproTelecom Service</Uptime>
   </soap:Body>
 </soap:Envelope>`,
                 ]);
